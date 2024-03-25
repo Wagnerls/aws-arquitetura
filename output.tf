6514157678864fa7d6c33564f01fe65687255d41
Wagner Lopes <wagnersilva1795@yahoo.com.br>
2024-03-25 19:26:55 -0300

    Adicionar arquivos de configuração do Terraform

---
+++ output.tf
@@ -0,0 +1,26 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.public_subnet_b.id
+  ]
+}
+
+output "load_balancer_dns_name" {
+  value = aws_elb.web-lb.dns_name
+}
+
+output "autoscaling_group_name" {
+  value = aws_autoscaling_group.data-processing-asg.name
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.test-nat-gateway.id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my-test-bucket.bucket
+}
